faeda18e86624f83e37385134ecf82886a4052c0
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2018-11-15 21:53:06 -0600

    finished example

--- main.js
+++ main.js
@@ -7,29 +7,32 @@ var play = myApp.querySelector('#play');
 // function que regrese piedra papel o tijera
 
 function rockPaperScissors(){
-  return Math.floor(Math.random() * 3)
+  return Math.floor(Math.random() * 3);
 }
 
-// function que evalue que jugador gano
-// game(player1, player2);
+// // function que evalue que jugador gano
+// // game(player1, player2);
 function game(){
   var player1 = rockPaperScissors();
   var player2 = rockPaperScissors();
   var options = ['rock', 'paper', 'scissors'];
-  if (player1 > player2 && player1 - player2 != 2)
-  render(options[player1], options[payer2], 'player1');
+
+  if (player1 > player2 && player1 - player2 != 2){
+    render(options[player1], options[player2], 'player1');
+  } else if (player2 > player1 && player2 - player1 != 2){
+    render(options[player1], options[player2], 'player2');
+  } else if (player1 === player2){
+    render(options[player1], options[player2], 'draw');
+  }
 }
 
-// function que dibuje en el DOM
-// resultados de los jugadores y quien gano
+// // function que dibuje en el DOM
+// // resultados de los jugadores y quien gano
 function render(player1, player2, hoWon){
   player1Area.textContent = player1;
   player2Area.textContent = player2;
   result.textContent = hoWon;
 }
 
-// render('paper', 'rock', 'player1');
-
 // declarar un eventlistener para el botón de jugar.
-
-game();
+play.addEventListener('click', game)
